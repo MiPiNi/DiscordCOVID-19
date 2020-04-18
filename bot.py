069594f0efa7811fd1e2f20e2de1be3331f2df05
Daniel Grala <YouTubePortoPL@wp.pl>
2020-04-18 02:31:58 +0200

    Added ?history
    Added command ?history - shows stats from last 7 days in PL.
    Splited showing world stats and country stats in to seperate commands ?korona <country> and ?world

--- bot.py
+++ bot.py
@@ -1,5 +1,5 @@
 import requests
-import json
+import discord
 import datetime
 from discord.ext import commands
 
@@ -13,70 +13,103 @@ KEY = str(open("KEY.txt").readline())
 async def on_ready():
     print("Ready!")
     print(datetime.date.today())
+    await bot.change_presence(activity=discord.Game(name="?help"))
 
 
-@bot.command(name="korona", help="Podaje statystyki koronawirusa w PL")
+@bot.command(name="korona", help="Podaje statystyki koronawirusa w danym panstwie.\n Uzycie: ?korona <country>")
 async def korona_stats(ctx, country='poland'):
     try:
-        if country == "world" or country == "World":
-            url = "https://coronavirus-monitor.p.rapidapi.com/coronavirus/worldstat.php"
+        url = "https://covid-193.p.rapidapi.com/statistics"
 
-            headers = {
-                'x-rapidapi-host': "coronavirus-monitor.p.rapidapi.com",
-                'x-rapidapi-key': KEY
-            }
+        querystring = {"country": country}
 
-            response = requests.request("GET", url, headers=headers)
-            data = response.json()
-            await ctx.send("**[" + country.capitalize() + "]**")
-            await ctx.send('**Przypadki:**\n \
-                Nowe przypadki: +{}\n \
-                Obecne przypadki: {}\n \
-                Wszystkie wyleczone przypadki: {}\n \
-                Wszystkie potwierdzone przypadki: {}\n \
+        headers = {
+            'x-rapidapi-host': "covid-193.p.rapidapi.com",
+            'x-rapidapi-key': KEY
+        }
+
+        response = requests.request("GET", url, headers=headers, params=querystring)
+        message = response.json()
+        json_response = message['response']
+        zero = json_response[0]
+        cases = zero['cases']
+        deaths = zero['deaths']
+        await ctx.send("**[" + country.capitalize() + "]**")
+        await ctx.send('**Przypadki:**\n \
+                        Nowe przypadki: {}\n \
+                        Obecne przypadki: {}\n \
+                        Przypadki krytyczne: {}\n \
+                        Wyleczone przypadki: {}\n \
+                        Wszystkie potwierdzone przypadki: {}\n \
 **Deduwy:**\n \
-                Nowe deduwy: +{}\n \
-                Laczne deduwy: {}'.format(data['new_cases'],
-                                          data['active_cases'],
-                                          data['total_recovered'],
-                                          data['total_cases'],
-                                          data['new_deaths'],
-                                          data['total_deaths']))
-
-        else:
-            url = "https://covid-193.p.rapidapi.com/statistics"
-
-            querystring = {"country": country}
-
-            headers = {
-                'x-rapidapi-host': "covid-193.p.rapidapi.com",
-                'x-rapidapi-key': KEY
-            }
-
-            response = requests.request("GET", url, headers=headers, params=querystring)
-            message = response.json()
-            json_response = message['response']
-            zero = json_response[0]
-            cases = zero['cases']
-            deaths = zero['deaths']
-            await ctx.send("**[" + country.capitalize() + "]**")
-            await ctx.send('**Przypadki:**\n \
-                Nowe przypadki: {}\n \
-                Obecne przypadki: {}\n \
-                Przypadki krytyczne: {}\n \
-                Wyleczone przypadki: {}\n \
-                Wszystkie potwierdzone przypadki: {}\n \
-**Deduwy:**\n \
-                Nowe deduwy: {}\n \
-                Laczne deduwy: {}'.format(cases['new'],
-                                          cases['active'],
-                                          cases['critical'],
-                                          cases['recovered'],
-                                          cases['total'],
-                                          deaths['new'],
-                                          deaths['total']))
+                        Nowe deduwy: {}\n \
+                        Laczne deduwy: {}'.format(cases['new'],
+                                                  cases['active'],
+                                                  cases['critical'],
+                                                  cases['recovered'],
+                                                  cases['total'],
+                                                  deaths['new'],
+                                                  deaths['total']))
     except IndexError:
         await ctx.send("Bledne panstwo!\nUzywaj angielskich nazw! (np. poland, usa, china)")
+
+
+@bot.command(name="world", help="Podaje statystyki koronawirusa na swiecie")
+async def world_stats(ctx):
+    url = "https://coronavirus-monitor.p.rapidapi.com/coronavirus/worldstat.php"
+
+    headers = {
+        'x-rapidapi-host': "coronavirus-monitor.p.rapidapi.com",
+        'x-rapidapi-key': KEY
+    }
+
+    response = requests.request("GET", url, headers=headers)
+    data = response.json()
+    await ctx.send("**[World]**")
+    await ctx.send('**Przypadki:**\n \
+                    Nowe przypadki: +{}\n \
+                    Obecne przypadki: {}\n \
+                    Wszystkie wyleczone przypadki: {}\n \
+                    Wszystkie potwierdzone przypadki: {}\n \
+**Deduwy:**\n \
+                    Nowe deduwy: +{}\n \
+                    Laczne deduwy: {}'.format(data['new_cases'],
+                                              data['active_cases'],
+                                              data['total_recovered'],
+                                              data['total_cases'],
+                                              data['new_deaths'],
+                                              data['total_deaths']))
+
+
+@bot.command(name="history",
+             help="Wyswietla statystyki z ostatnich 7 dni w Polsce.")
+async def korona_history(ctx,):
+    today = datetime.datetime.today()
+    week_ago = str(datetime.datetime(year=today.year, month=today.month, day=today.day - 7, hour=0))
+    day_before = str(datetime.datetime(year=today.year, month=today.month, day=today.day, hour=0))
+    url = "https://api.covid19api.com/country/poland" + "?from=" + week_ago + "&to=" + day_before
+
+    response = requests.request("GET", url)
+
+    message = response.json()
+    i = 0
+
+    await ctx.send("**[Poland]**")
+
+    while i <= 6:
+        data = message[i]
+        active = data['Confirmed'] - (data['Recovered'] + data['Deaths'])
+        await ctx.send('***' + str(data['Date'])[:10] + '***\n \
+**Przypadki:**\n \
+                        Aktywne przypadki: {}\n \
+                        Wyleczone przypadki: {}\n \
+                        Wszystkie potwierdzone przypadki: {}\n \
+**Deduwy:**\n \
+                        Deduwy: {}\n'.format(active,
+                                           data['Recovered'],
+                                           data['Confirmed'],
+                                           data['Deaths']))
+        i += 1
 
 
 bot.run(TOKEN)
